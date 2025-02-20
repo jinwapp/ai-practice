f4e758b646dbc7a738e8e3cb0393fda192f98fb3
jinwapp <104186487+jinwapp@users.noreply.github.com>
2025-02-20 22:50:33 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# from langchain_openai import ChatOpenAI
+# chat_model = ChatOpenAI()
+
+# content = '코딩'
+
+# result = chat_model.predict(content + '에 대한 시를 써줘')
+# print(result)
+
+
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
